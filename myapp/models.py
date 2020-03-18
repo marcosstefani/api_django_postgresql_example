f4004f528c5aad803e0ad9749b204a2407829fa5
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2020-03-18 02:09:53 -0300

    postgres migration

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=100, blank=False)
     last_name = models.CharField(max_length=100, blank=False)
-    email = models.CharField(max_length=200, blank=False, unique=True)
+    email = models.CharField(max_length=200, blank=False, unique=True, primary_key=True)
     birthday = models.DateField(blank=False)
 
     def __str__(self):
